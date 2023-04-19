ff152351baf63d6bc3528a8abb4266f257808522
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-04-19 14:46:32 +0530

    finally done

---
+++ src/financemangement/FinanceMangement.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package financemangement;
+
+/**
+ *
+ * @author 91861
+ */
+public class FinanceMangement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Financeloginsignup.LoginPage h = new Financeloginsignup.LoginPage();
+        h.setVisible(true);
+    }
+    
+}
